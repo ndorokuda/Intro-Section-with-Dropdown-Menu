d8f1d4e0a65ba4392eebcbca844da912756e6738
Kudakwashe Ndoro <ndorokuda53@gmail.com>
2024-05-17 22:07:03 +0200

    Deployed Site

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Intro-Section-with-Dropdown-Menu',
   plugins: [react()],
   server: {
     port: 3000,
